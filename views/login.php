737506a0e87ce80c33478d7ee0a44355796c8c7a
mhoj <martin@hojman.com.ar>
2024-09-04 01:15:43 -0300

    Start Login form view

--- views/login.php
+++ views/login.php
@@ -3,8 +3,15 @@
 <div class="w-1/2 bg-black h-full">
     <img class="h-full" src="https://cdn.pixabay.com/photo/2024/02/22/05/40/natural-scenery-8589165_640.jpg" />
 </div>
-<div class="w-1/2 items-center">
-    <form >
-        <h2 class="text-center">Login</h2>
-    </form>
+<div class="w-1/2 items-center flex flex-col">
+    <h2 class="text-center font-bold mb-5">Login</h2>
+    <div class="border border-black border-solid flex justify-center w-1/2">
+
+        <form class="flex flex-col justify-center text-center" >
+            <label for="name" class="font-xl">Name/Username:</label>
+            <input name="name" type="text" />
+            <label for="password" class="font-xl">Password:</label>
+            <input name="password" type="password" />
+        </form>
+    </div>
 </div>
